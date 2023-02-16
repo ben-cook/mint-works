7d0cecac9fbd6de23aa783d21af2b00427f8c143
Ben Cook <mcbeanoo@gmail.com>
2023-02-16 18:14:03 +0700

    add more logging config

--- logger.ts
+++ logger.ts
@@ -1,5 +1,15 @@
 import * as log from "https://deno.land/std@0.177.0/log/mod.ts";
 
-log.setup({});
+log.setup({
+  handlers: {
+    consoleHandler: new log.handlers.ConsoleHandler("DEBUG"),
+  },
+  loggers: {
+    default: {
+      level: "DEBUG",
+      handlers: ["consoleHandler"],
+    },
+  },
+});
 
 export const logger = log.getLogger();
