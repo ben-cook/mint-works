010d564015f1313c051984210fe7ab85ad247b75
Aerilym <aerilymbiz@gmail.com>
2023-02-16 16:10:33 +0700

    Location class

--- location.ts
+++ location.ts
@@ -1,9 +1,31 @@
+class Slot {
+  basePrice: number;
+  tokens: number;
+
+  constructor(basePrice: number, tokens: number) {
+    this.basePrice = basePrice;
+    this.tokens = tokens;
+  }
+  public available(): boolean {
+    return this.tokens < 1;
+  }
+}
+
+type LocationType = "Core" | "Deed" | "Advanced";
+
 class LocationCard {
   name: string;
-  description: string;
+  type: LocationType;
+  effect: string;
+  slots: Slot[];
 
-  constructor(name: string, description: string) {
+  constructor(name: string, type: LocationType, effect: string, slots: Slot[]) {
     this.name = name;
-    this.description = description;
+    this.type = type;
+    this.effect = effect;
+    this.slots = slots;
+  }
+  public available(): boolean {
+    return this.slots.some((slot) => slot.available());
   }
 }
