6fc44bdcd2ca7fcc2fcdaa9a24f69fb3b035a2d7
Ben Cook <mcbeanoo@gmail.com>
2023-02-17 09:47:07 +0700

    add filesystem logging

--- logger.ts
+++ logger.ts
@@ -3,11 +3,15 @@ import * as log from "https://deno.land/std@0.177.0/log/mod.ts";
 log.setup({
   handlers: {
     consoleHandler: new log.handlers.ConsoleHandler("DEBUG"),
+    fileHandler: new log.handlers.FileHandler("DEBUG", {
+      filename: "./log.txt",
+      mode: "w",
+    }),
   },
   loggers: {
     default: {
       level: "DEBUG",
-      handlers: ["consoleHandler"],
+      handlers: ["consoleHandler", "fileHandler"],
     },
   },
 });
