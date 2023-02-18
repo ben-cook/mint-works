547396305b165341776bdd375bbb33b87ddd9147
Ben Cook <mcbeanoo@gmail.com>
2023-02-18 14:40:21 +0700

    add optional update() method to player interface

--- player.ts
+++ player.ts
@@ -2,6 +2,12 @@ import { State } from "./state.ts";
 import { Turn } from "./turn.ts";
 
 export interface Player {
-  /** Make a move */
-  takeTurn(state: State): Promise<Turn>;
+  /** The `takeTurn` method is called when it is this player's turn.
+   * @returns A promise that resolves to the turn that the player chooses
+   */
+  takeTurn: (state: State) => Promise<Turn>;
+  /** The `update` method is called to inform a given player of the other player's moves.
+   * This could be used to update a UI, to train a model, etc.
+   */
+  update?: (turn: Turn) => void;
 }
