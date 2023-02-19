871c78f1d88396c8f7d217862479e34187682d28
Ben Cook <mcbeanoo@gmail.com>
2023-02-19 09:31:36 +0700

    update test names

--- mint_works.test.ts
+++ mint_works.test.ts
@@ -3,27 +3,29 @@ import { Neighbourhood } from "./neighbourhood.ts";
 import { RandomPlayer } from "./players/random_player.ts";
 import { assertEquals } from "https://deno.land/std@0.177.0/testing/asserts.ts";
 
-Deno.test("Has plans", () => {
-  const mintWorks = new MintWorks();
-  mintWorks.players = [
-    {
-      label: "Test Player 1",
-      age: 21,
-      neighbourhood: new Neighbourhood(),
-      player: new RandomPlayer(),
-      tokens: 5,
-    },
-    {
-      label: "Test Player 2",
-      age: 34,
-      neighbourhood: new Neighbourhood(),
-      player: new RandomPlayer(),
-      tokens: 5,
-    },
-  ];
-  mintWorks.players[0].neighbourhood.addPlan("Windmill");
-  assertEquals(
-    mintWorks.players[0].neighbourhood.getPlan("Windmill")?.name,
-    "Windmill",
-  );
+Deno.test("MintWorks", async (t) => {
+  await t.step("Has plans", () => {
+    const mintWorks = new MintWorks();
+    mintWorks.players = [
+      {
+        label: "Test Player 1",
+        age: 21,
+        neighbourhood: new Neighbourhood(),
+        player: new RandomPlayer(),
+        tokens: 5,
+      },
+      {
+        label: "Test Player 2",
+        age: 34,
+        neighbourhood: new Neighbourhood(),
+        player: new RandomPlayer(),
+        tokens: 5,
+      },
+    ];
+    mintWorks.players[0].neighbourhood.addPlan("Windmill");
+    assertEquals(
+      mintWorks.players[0].neighbourhood.getPlan("Windmill")?.name,
+      "Windmill",
+    );
+  });
 });
