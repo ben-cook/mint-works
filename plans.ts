c1ef84d3c497290ab164d95cf688e5c107004004
Ben Cook <mcbeanoo@gmail.com>
2023-02-19 10:22:55 +0700

    add check for crane when building

--- plans.ts
+++ plans.ts
@@ -63,6 +63,13 @@ const plans = [
     // TODO: implement pay less at supplier
     effect: undefined,
   },
+  {
+    name: "Crane",
+    cost: 2,
+    baseStars: 1,
+    // TODO: implement pay less at builder
+    effect: undefined,
+  },
 ] as const satisfies ReadonlyArray<Plan>;
 
 export const createPlans = (): Array<Plan> => [...plans];
--- player.ts
+++ player.ts
@@ -27,7 +27,7 @@ export class PlayerHelper {
       playerName: this.name,
     }];
     const builder = state.locations.find((l) => l.name === "Builder");
-    if (builder?.available()) {
+    if (builder?.available() && this.canAffordToBuild(state)) {
       for (
         const plan of state.players.find((p) => p.label === this.name)!
           .neighbourhood.plans
@@ -42,5 +42,20 @@ export class PlayerHelper {
       }
     }
     return validTurns;
+  }
+
+  canAffordToBuild(state: State): boolean {
+    const player = this.thisPlayer(state);
+
+    if (player.neighbourhood.getBuilding("Crane")) {
+      return player.tokens >= 1;
+    }
+
+    return player.tokens >= 2;
+  }
+
+  /** Return this player's information from a state object */
+  thisPlayer(state: State) {
+    return state.players.find((p) => p.label === this.name)!;
   }
 }
