42c2e3135c4a17fc205bc14885c3aed8e48dd1e2
Aerilym <aerilymbiz@gmail.com>
2023-02-19 18:05:50 +0700

    Add scoring tests

--- scoring.test.ts
+++ scoring.test.ts
@@ -4,12 +4,14 @@ import { Neighbourhood } from "./neighbourhood.ts";
 import { RandomPlayer } from "./players/random_player.ts";
 import { findWinner } from "./scoring.ts";
 
-Deno.test("Scoring", () => {
+Deno.test("Scoring", async (scoreTest) => {
   const mintWorks = new MintWorks();
   const playerOneName = "Test Player 1";
   const playerTwoName = "Test Player 2";
+  const playerThreeName = "Test Player 3";
   const playerOneAge = 21;
   const playerTwoAge = 34;
+  const playerThreeAge = 42;
   mintWorks.players = [
     {
       label: playerOneName,
@@ -25,10 +27,36 @@ Deno.test("Scoring", () => {
       player: new RandomPlayer(playerTwoName),
       tokens: 5,
     },
+    {
+      label: playerThreeName,
+      age: playerThreeAge,
+      neighbourhood: new Neighbourhood(),
+      player: new RandomPlayer(playerThreeName),
+      tokens: 5,
+    },
   ];
-  mintWorks.players.find((p) => p.label === playerOneName)!.neighbourhood
-    .addBuilding("Windmill");
-  mintWorks.players.find((p) => p.label === playerTwoName)!.neighbourhood
-    .addBuilding("Statue");
-  assertEquals(findWinner(mintWorks.players).winner, playerTwoName);
+  await scoreTest.step("Star victory", () => {
+    mintWorks.players.find((p) => p.label === playerOneName)!.neighbourhood
+      .addBuilding("Windmill");
+    mintWorks.players.find((p) => p.label === playerTwoName)!.neighbourhood
+      .addBuilding("Statue");
+    assertEquals(findWinner(mintWorks.players).winner, playerTwoName);
+  });
+  await scoreTest.step("Neighbourhood size victory", () => {
+    mintWorks.players.find((p) => p.label === playerThreeName)!.neighbourhood
+      .addBuilding("Statue");
+    mintWorks.players.find((p) => p.label === playerThreeName)!.neighbourhood
+      .addPlan("Gardens");
+    assertEquals(findWinner(mintWorks.players).winner, playerThreeName);
+  });
+  await scoreTest.step("Token victory", () => {
+    mintWorks.players.find((p) => p.label === playerTwoName)!.neighbourhood
+      .addPlan("Crane");
+    mintWorks.players.find((p) => p.label === playerTwoName)!.tokens += 5;
+    assertEquals(findWinner(mintWorks.players).winner, playerTwoName);
+  });
+  await scoreTest.step("Token victory", () => {
+    mintWorks.players.find((p) => p.label === playerThreeName)!.tokens += 5;
+    assertEquals(findWinner(mintWorks.players).winner, playerThreeName);
+  });
 });
