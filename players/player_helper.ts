fdb095ce3d7fde000f67f376a4362f67e8ea8dd3
Aerilym <aerilymbiz@gmail.com>
2023-02-20 14:10:16 +0700

    Refactor turn generator cost lookups to use a generic method

--- players/player_helper.ts
+++ players/player_helper.ts
@@ -1,3 +1,4 @@
+import { LocationCard } from "../location.ts";
 import { State } from "../state.ts";
 import { Turn } from "../turn.ts";
 
@@ -17,7 +18,7 @@ export class PlayerHelper {
     }];
 
     const builder = state.locations.find((l) => l.name === "Builder");
-    if (builder?.available() && this.canAffordToBuild(state)) {
+    if (builder?.available() && this.canAffordToBuild(state, builder)) {
       for (
         const plan of state.players.find((p) => p.label === this.name)!
           .neighbourhood.plans
@@ -33,7 +34,7 @@ export class PlayerHelper {
     }
 
     const producer = state.locations.find((l) => l.name === "Producer");
-    if (producer?.available()) {
+    if (producer?.available() && this.canAffordBasicLocation(state, producer)) {
       validTurns.push(
         {
           action: {
@@ -45,7 +46,9 @@ export class PlayerHelper {
     }
 
     const leadership = state.locations.find((l) => l.name === "Leadership");
-    if (leadership?.available()) {
+    if (
+      leadership?.available() && this.canAffordBasicLocation(state, leadership)
+    ) {
       validTurns.push(
         {
           action: {
@@ -79,20 +82,28 @@ export class PlayerHelper {
   }
 
   /** Return if the player can afford to build any of their plans */
-  canAffordToBuild(state: State): boolean {
+  canAffordToBuild(state: State, builder: LocationCard): boolean {
     const player = this.thisPlayer(state);
 
+    let cost = builder.minSlotPrice();
+
     if (player.neighbourhood.getBuilding("Crane")) {
-      return player.tokens >= 1;
+      cost -= 1;
     }
 
-    return player.tokens >= 2;
+    return player.tokens >= cost;
   }
 
   /** Return if the player can afford to supply any of the plan supply plans */
   canAffordToSupply(state: State): boolean {
     const player = this.thisPlayer(state);
     return state.planSupply.some((p) => p.cost <= player.tokens);
+  }
+
+  /** Return if the player can afford to use a basic location */
+  canAffordBasicLocation(state: State, location: LocationCard): boolean {
+    const player = this.thisPlayer(state);
+    return player.tokens >= location.minSlotPrice();
   }
 
   /** Return this player's information from a state object */
