3aa31dd1c3fefa9021f8995724ed967c8fb74190
Aerilym <aerilymbiz@gmail.com>
2023-04-22 17:06:08 +1000

    Create mint works turn factory

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 import { InteractionHooks, InterfacePlayer } from "./players/interface_player";
-import { MintWorksEngine, PlayerWithInformation } from "./mint_works";
+import {
+  MintWorksEngine,
+  MintWorksEngineState,
+  MintWorksParams,
+  PlayerWithInformation,
+} from "./mint_works";
 import { Neighbourhood } from "./neighbourhood";
+import { PlayerHelper } from "./players/player_helper";
 
 /**
  *
@@ -84,3 +90,57 @@ export class MintWorks {
     return true;
   }
 }
+
+/**
+ * A factory to create turns for a player
+ */
+export class MintWorksTurnFactory {
+  private playerHelper: PlayerHelper;
+  private state: MintWorksEngineState;
+  /**
+   * Create a turn factory
+   * @param playerName - The name of the player to create turns for
+   * @param state - The state of the game
+   *
+   * @remarks
+   * If no player name is provided, the player to take turn will be used. If no player to take turn is provided, the starting player token will be used.
+   */
+  constructor({ playerName, state }: { playerName?: string; state: MintWorksEngineState }) {
+    const targetPlayerName = state.playerToTakeTurn ?? playerName ?? state.startingPlayerToken;
+    if (!targetPlayerName) throw new Error("No player to take turn");
+
+    this.state = state;
+    this.playerHelper = new PlayerHelper(targetPlayerName);
+  }
+
+  /**
+   * Get the turns that the player can take.
+   * @returns The turns that the player can take.
+   */
+  public getTurns(): Array<Turn> {
+    return this.playerHelper.generateTurns(this.state);
+  }
+
+  /**
+   * Validate a turn for a player in a given state
+   * @param turn - The turn to validate
+   * @param state - The state of the game
+   * @returns True if the turn is valid
+   */
+  public static validateTurn({
+    turn,
+    state,
+  }: {
+    turn: Turn;
+    state: MintWorksEngineState;
+  }): boolean {
+    const turnFactory = new MintWorksTurnFactory({
+      playerName: turn.playerName,
+      state: state,
+    });
+    const turns = turnFactory.getTurns();
+
+    return turns.map((turn) => JSON.stringify(turn)).includes(JSON.stringify(turn));
+  }
+}
+
